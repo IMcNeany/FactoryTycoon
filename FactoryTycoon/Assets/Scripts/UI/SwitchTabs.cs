481b5884330d68cf8e5bcac4e376b1c90e644ee7
Isobel McNeany <icmc112@yahoo.co.uk>
2019-12-18 22:17:34 +0000

    Created main upgrade tabs and allows changing between them

---
+++ FactoryTycoon/Assets/Scripts/UI/SwitchTabs.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchTabs : MonoBehaviour
+{
+    public List<GameObject> tabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OpenTab(0);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenTab(int tabNo)
+    {
+        for(int i = 0; i < tabs.Count; i++)
+        {
+            if(tabNo == i)
+            {
+                tabs[i].SetActive(true);
+             
+            }
+            else
+            {
+                tabs[i].SetActive(false);
+               
+            }
+        }
+    }
+}
