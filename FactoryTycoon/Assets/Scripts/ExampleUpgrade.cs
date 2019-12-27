6cea6f6e3216e2bb6bf9c1abd773fc86cef0a81d
Isobel McNeany <icmc112@yahoo.co.uk>
2019-12-27 19:09:45 +0000

    Started pop up for machines/upgrades

---
+++ FactoryTycoon/Assets/Scripts/ExampleUpgrade.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExampleUpgrade : Upgrades
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        title = "item Title";
+        description = "content description of upgrade/machine" +
+            "Social: +2" +
+            "Ecomical: +3" +
+            "Enviromental: -2";
+        cost = 100000;
+
+    }
+
+    void UpdatePanel()
+    {
+        //turn to list
+        TextMeshProUGUI titleTextBox =  panel.transform.GetChild(0).transform.GetComponentInChildren<TextMeshProUGUI>();
+        if(titleTextBox.gameObject.name == "Title")
+        {
+            titleTextBox.SetText(title);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FactoryTycoon/Assets/Scripts/Upgrades.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrades : MonoBehaviour
+{
+    protected string title;
+    protected string description;
+    protected Sprite image;
+    protected float cost;
+    protected GameObject panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel = Resources.Load("Panel") as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
